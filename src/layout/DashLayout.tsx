6a1d4656453b4ae332691267453168960c2f92ee
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-12-02 15:09:59 +0100

    Update file paths and dependencies

--- src/layout/DashLayout.tsx
+++ src/layout/DashLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { SideBar } from "../components/SideBar";
+import { SideBar } from "@/components/SideBar";
 
 export const DashLayout = () => {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
-      ...(process.env.NODE_ENV !== "development"
-        ? { "./runtimeConfig": "./runtimeConfig.browser" }
-        : {}),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
