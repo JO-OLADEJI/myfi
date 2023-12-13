a97158438c32a0d8930883d44a9ca770e0cd38e4
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-12-13 22:46:50 +0100

    connect banks

--- src/assets/banks-logo/kuda.tsx
+++ src/assets/banks-logo/kuda.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Kuda = () => {
   return (
     <svg
--- src/components/ConnectBanks.tsx
+++ src/components/ConnectBanks.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { Button } from "./ui/Button";
 import { Plus } from "@/assets/banks-connected";
-import { getBankLogo } from "@/lib/utils";
 import useAppStore from "@/contexts/state";
+import { getBankLogo } from "@/lib/utils";
+import { Button } from "./ui/Button";
 
 export const ConnectBanks = () => {
   const connectedBanks = useAppStore((state) => state.connectedBanks);
@@ -10,15 +9,18 @@ export const ConnectBanks = () => {
   return (
     <div className="bg-white p-6 flex flex-col gap-3 rounded-3xl">
       <p className="text-neutral-400 text-lg font-normal">Connected Banks</p>
-      <div className="border flex">
+      <div className=" flex">
         <Button className="text-blue-700 text-base font-normal mr-5">
           <div className="p-4 rounded-full border-2 border-dashed border-blue-700">
             <Plus />
           </div>
         </Button>
-        <div className="w-14 .flex .items-center .justify-center">
+        <div className="w-14 flex items-center  gap-4">
           {connectedBanks.map((bank, index) => (
-            <div key={index} className="border">
+            <div
+              key={index}
+              className=" flex border p-2 rounded-full items-center justify-center min-w-[60px] min-h-[50px] h-full"
+            >
               {getBankLogo(bank)}
             </div>
           ))}
